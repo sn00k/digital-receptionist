b27a2dadb3f491650b9fd588c9cef59b0ce182b6
sn00k <robin@robins.nu>
2021-09-03 15:15:59 +0200

    move i18n translations to util file

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,35 +8,7 @@ import Card from "../components/Card";
 import { useForm } from "react-hook-form";
 import Link from "next/link";
 import Logo from "../components/Logo";
-
-const mainContent: { [key: string]: any } = {
-  en: {
-    landingTitle: "Digital Receptionist",
-    greeting: "Hi, welcome to the digital receptionist!",
-    startButton: "Start",
-    form: {
-      title: "Please enter your information below",
-      firstName: "First name",
-      lastName: "Last name",
-      email: "Email",
-      company: "Company",
-      submit: "Send",
-    },
-  },
-  sv: {
-    landingTitle: "Digital Receptionist",
-    greeting: "Hej, välkommen till den digitala receptionisten!",
-    startButton: "Starta",
-    form: {
-      title: "Vänligen fyll i dina uppgifter nedan",
-      firstName: "Förnamn",
-      lastName: "Efternamn",
-      email: "Epost",
-      company: "Företag",
-      submit: "Skicka",
-    },
-  },
-};
+import { textContent } from "../utils/sv_us";
 
 const employees: object[] = [
   {
@@ -59,7 +31,8 @@ const Home: NextPage = () => {
     return null;
   }
 
-  const { landingTitle, greeting, startButton, form } = mainContent[locale];
+  console.log("textContent: ", textContent);
+  const { landingTitle, greeting, startButton, form } = textContent[locale];
 
   const onSubmit = (data: any) => {
     console.log("data: ", data);
---
+++ utils/sv_us.ts
@@ -0,0 +1,28 @@
+export const textContent: { [key: string]: any } = {
+  en: {
+    landingTitle: "Digital Receptionist",
+    greeting: "Hi, welcome to the digital receptionist!",
+    startButton: "Start",
+    form: {
+      title: "Please enter your information below",
+      firstName: "First name",
+      lastName: "Last name",
+      email: "Email",
+      company: "Company",
+      submit: "Send",
+    },
+  },
+  sv: {
+    landingTitle: "Digital Receptionist",
+    greeting: "Hej, välkommen till den digitala receptionisten!",
+    startButton: "Starta",
+    form: {
+      title: "Vänligen fyll i dina uppgifter nedan",
+      firstName: "Förnamn",
+      lastName: "Efternamn",
+      email: "Epost",
+      company: "Företag",
+      submit: "Skicka",
+    },
+  },
+};
