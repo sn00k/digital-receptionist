0a628e04a57551b6308bd296c89074df9a954df8
sn00k <robin@robins.nu>
2021-09-28 16:35:03 +0200

    add Head to _app page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import "reflect-metadata";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;1,100&display=swap"
+          rel="stylesheet"
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 export default MyApp;
